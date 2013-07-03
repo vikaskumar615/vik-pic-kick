5befd5b59a5cc3ae8fb552f222d7f994772d6a04
vikaskumar615 <vikaskumar615@gmail.com>
2013-07-03 20:03:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<html><title>FullOnSms multi Wap Sender | vikas</title><form action='sendsms.php' method='post'><br><b>Username :<br><input type='text' name='uid'><br>Password :<br><input type='text' name='pwd'><br>Recipients :<br>[Seprated By ','  'comma']<br><input type='text' name='to'><br>Message :</b><br><textarea name='sms'></textarea><br><input type='submit' name='submit' value='Send'></form></html> 
+<html><title>FullOnSms multi Wap Sender | vikas</title>
+<form action='sendsms.php' method='post'><br> //replace sendsms.php with the php of corresponding api
+<b>Username :<br><input type='text' name='uid'>
+<br>Password :<br><input type='text' name='pwd'><br>
+Recipients :<br>[Seprated By ','  'comma']<br>
+<input type='text' name='to'><br>Message :</b><br>
+<textarea name='sms'></textarea><br>
+<input type='submit' name='submit' value='Send'></form></html> 
